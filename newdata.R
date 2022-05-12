fffa9b85ae48f3ed2b8182834ea88c0ba06a1b83
muhammad ijaz <miitpro786@gmail.com>
2022-05-11 21:44:07 -0700

    newfile addded

---
+++ newdata.R
@@ -0,0 +1,10 @@
+pets <=c("at","mouse","dog")
+weigh <- c(5,2,9)
+feed <- c("yes", "no")
+run <- c(2,8,10)
+house.pet<- data.frame(type=pets,weigh,feed,run)
+install.packages("sm")
+fruits<= c("banana","mango","ornage","ach","pear")
+x <- 13
+y <- 23
+z <-x*y
